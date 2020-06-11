3e54ab75429b036ca6d3e17579493c0161d8d79d
421342531@qq.com <421342531@qq.com>
2020-06-12 02:00:05 +0800

    0612

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -36,8 +36,11 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-    	
-        response.getWriter().append("请重新登陆！");//.append(request.getContextPath());
+		ServletContext sc = getServletContext();  
+		RequestDispatcher rd = null;   
+		
+		rd = sc.getRequestDispatcher("/index.jsp"); //定向的页面   
+		rd.forward(request, response);
 	}
 
 	/**
